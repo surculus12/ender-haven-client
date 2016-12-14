917d3d3fb9515b5619c4a44f9306ef8c65f675ce
romovs <romovs@gmail.com>
2016-12-14 05:08:44 +0200

    Cleanup hitbox rotation

--- src/haven/GobHitbox.java
+++ src/haven/GobHitbox.java
@@ -4,7 +4,6 @@ import javax.media.opengl.GL2;
 import java.awt.*;
 
 public class GobHitbox extends Sprite {
-    private final static float center = 0.5f;
     private static final States.ColState fillclrstate = new States.ColState(new Color(114, 159, 207, 200));
     private static final States.ColState bbclrstate = new States.ColState(new Color(255, 255, 255, 255));
     private Coordf a, b, c, d;
@@ -22,22 +21,10 @@ public class GobHitbox extends Sprite {
             clrstate = bbclrstate;
         }
 
-        // rotate around map pixel's center
-        double cos = Math.cos(-gob.a);
-        double sin = Math.sin(-gob.a);
-
-        a = rotate(ac.x, ac.y, center, -center, cos, sin);
-        b = rotate(ac.x, bc.y, center, -center, cos, sin);
-        c = rotate(bc.x, bc.y, center, -center, cos, sin);
-        d = rotate(bc.x, ac.y, center, -center, cos, sin);
-
-        // because overlay is rotated according to gob.a during rendering
-        // we rotate it in the opposite direction first to negate the effect
-        sin = -sin; // reverse the angle. cos(a) == cos(-a) hence no need to touch it.
-        a = rotate(a.x, a.y, cos, sin);
-        b = rotate(b.x, b.y, cos, sin);
-        c = rotate(c.x, c.y, cos, sin);
-        d = rotate(d.x, d.y, cos, sin);
+        a = new Coordf(ac.x, ac.y);
+        b = new Coordf(ac.x, bc.y);
+        c = new Coordf(bc.x, bc.y);
+        d = new Coordf(bc.x, ac.y);
     }
 
     public boolean setup(RenderList rl) {
@@ -65,16 +52,6 @@ public class GobHitbox extends Sprite {
             gl.glVertex3f(b.x, b.y, 1);
         }
         gl.glEnd();
-    }
-
-    private Coordf rotate(float x, float y, double cos, double sin) {
-        return new Coordf((float) (x * cos - y * sin), (float) (x * sin + y * cos));
-    }
-
-    private Coordf rotate(float x, float y, float pivotx, float pivoty, double cos, double sin) {
-        x -= pivotx;
-        y -= pivoty;
-        return new Coordf((float) ((x * cos - y * sin) + pivotx), (float) ((x * sin + y * cos) + pivoty));
     }
 
     public static class BBox {
