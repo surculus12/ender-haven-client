244760de8723c879d1e1a72f4c4bfda8df4225d8
romovs <romovs@gmail.com>
2017-03-04 12:52:16 +0200

    Update icons list

--- src/haven/Config.java
+++ src/haven/Config.java
@@ -279,7 +279,7 @@ public class Config {
         put("almondtree", new CheckListboxItem("Almond"));
     }};
 
-    public final static HashMap<String, CheckListboxItem> icons = new HashMap<String, CheckListboxItem>(28) {{
+    public final static HashMap<String, CheckListboxItem> icons = new HashMap<String, CheckListboxItem>(30) {{
         put("dandelion", new CheckListboxItem("Dandelion"));
         put("chantrelle", new CheckListboxItem("Chantrelle"));
         put("blueberry", new CheckListboxItem("Blueberry"));
@@ -309,6 +309,8 @@ public class Config {
         put("rabbit", new CheckListboxItem("Rabbit"));
         put("lingon", new CheckListboxItem("Lingonberries"));
         put("grub", new CheckListboxItem("Grub"));
+        put("yellowfoot", new CheckListboxItem("Yellowfoot"));
+        put("chives", new CheckListboxItem("Chives"));
     }};
 
     public final static HashMap<String, CheckListboxItem> flowermenus = new HashMap<String, CheckListboxItem>(13) {{
