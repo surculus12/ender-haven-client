c380db1a8b10af5be5a04f648995de6d116c20ef
romovs <romovs@gmail.com>
2017-07-08 18:23:59 +0300

    Add crab to icons list

--- src/haven/Config.java
+++ src/haven/Config.java
@@ -277,7 +277,7 @@ public class Config {
         put("almondtree", new CheckListboxItem("Almond"));
     }};
 
-    public final static HashMap<String, CheckListboxItem> icons = new HashMap<String, CheckListboxItem>(33) {{
+    public final static HashMap<String, CheckListboxItem> icons = new HashMap<String, CheckListboxItem>(34) {{
         put("dandelion", new CheckListboxItem("Dandelion"));
         put("chantrelle", new CheckListboxItem("Chantrelle"));
         put("blueberry", new CheckListboxItem("Blueberry"));
@@ -312,6 +312,7 @@ public class Config {
         put("rustroot", new CheckListboxItem("Rustroot"));
         put("boostspeed", new CheckListboxItem("Speed Boost"));
         put("adder", new CheckListboxItem("Adder"));
+        put("crab", new CheckListboxItem("Crab"));
     }};
 
     public final static HashMap<String, CheckListboxItem> flowermenus = new HashMap<String, CheckListboxItem>(15) {{
