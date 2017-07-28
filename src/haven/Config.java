f08b4ca184bc54ae406b61f403939a4576ec742c
romovs <romovs@gmail.com>
2017-07-28 23:34:07 +0300

    Add lady's mantle to icons list

--- src/haven/Config.java
+++ src/haven/Config.java
@@ -262,7 +262,7 @@ public class Config {
         put("almondtree", new CheckListboxItem("Almond"));
     }};
 
-    public final static HashMap<String, CheckListboxItem> icons = new HashMap<String, CheckListboxItem>(35) {{
+    public final static HashMap<String, CheckListboxItem> icons = new HashMap<String, CheckListboxItem>(36) {{
         put("dandelion", new CheckListboxItem("Dandelion"));
         put("chantrelle", new CheckListboxItem("Chantrelle"));
         put("blueberry", new CheckListboxItem("Blueberry"));
@@ -299,6 +299,7 @@ public class Config {
         put("adder", new CheckListboxItem("Adder"));
         put("crab", new CheckListboxItem("Crab"));
         put("clover", new CheckListboxItem("Clover"));
+        put("ladysmantle", new CheckListboxItem("Lady's Mantle"));
     }};
 
     public final static HashMap<String, CheckListboxItem> flowermenus = new HashMap<String, CheckListboxItem>(15) {{
