7fc0edb31fd74f9a109d8f0e36f0156b918aa6a3
romovs <romovs@gmail.com>
2017-09-06 22:09:11 +0300

    Add grasshoper to icons list

--- src/haven/Config.java
+++ src/haven/Config.java
@@ -263,7 +263,7 @@ public class Config {
         put("almondtree", new CheckListboxItem("Almond"));
     }};
 
-    public final static HashMap<String, CheckListboxItem> icons = new HashMap<String, CheckListboxItem>(36) {{
+    public final static HashMap<String, CheckListboxItem> icons = new HashMap<String, CheckListboxItem>(37) {{
         put("dandelion", new CheckListboxItem("Dandelion"));
         put("chantrelle", new CheckListboxItem("Chantrelle"));
         put("blueberry", new CheckListboxItem("Blueberry"));
@@ -301,6 +301,7 @@ public class Config {
         put("crab", new CheckListboxItem("Crab"));
         put("clover", new CheckListboxItem("Clover"));
         put("ladysmantle", new CheckListboxItem("Lady's Mantle"));
+        put("grasshopper", new CheckListboxItem("Grasshopper"));
     }};
 
     public final static HashMap<String, CheckListboxItem> flowermenus = new HashMap<String, CheckListboxItem>(16) {{
