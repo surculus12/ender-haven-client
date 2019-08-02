2d29f44be12ecbacb3863e728d66441da850e069
romovs <romovs@gmail.com>
2019-08-02 22:57:26 +0300

    Merge upstream

--- src/haven/Sprite.java
+++ src/haven/Sprite.java
@@ -28,6 +28,7 @@ package haven;
 
 import java.util.*;
 import java.lang.reflect.Constructor;
+import java.util.function.Function;
 
 public abstract class Sprite implements Rendered {
     public static final int GOB_HEALTH_ID = -1001;
@@ -55,6 +56,14 @@ public abstract class Sprite implements Rendered {
         public Factory make(Class<?> cl) throws InstantiationException, IllegalAccessException {
             if (Factory.class.isAssignableFrom(cl))
                 return (cl.asSubclass(Factory.class).newInstance());
+            try {
+                Function<Object[], Sprite> make = Utils.smthfun(cl, "mksprite", Sprite.class, Owner.class, Resource.class, Message.class);
+                return(new Factory() {
+                    public Sprite create(Owner owner, Resource res, Message sdt) {
+                        return(make.apply(new Object[] {owner, res, sdt}));
+                    }
+                });
+            } catch(NoSuchMethodException e) {}
             if (Sprite.class.isAssignableFrom(cl))
                 return (mkdynfact(cl.asSubclass(Sprite.class)));
             return (null);
