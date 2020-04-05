1046ece73fd56c268f4e558756df3a9a4768cc1f
Andrew Leap <andyleap@gmail.com>
2020-04-04 17:05:35 -0700

    Fix marker upload issues

--- src/haven/GameUI.java
+++ src/haven/GameUI.java
@@ -587,8 +587,11 @@ public class GameUI extends ConsoleHost implements Console.Directory {
                     RemoteNavigation.getInstance().uploadMarkerData(file);
                 if(Config.vendanMapv4) {
                     MappingClient.getInstance().ProcessMap(file, (m) -> {
-                        if(m instanceof PMarker && Config.vendanGreenMarkers) {
-                            return ((PMarker)m).color.equals(Color.GREEN);
+                        if(m instanceof PMarker) {
+                            if (Config.vendanGreenMarkers) {
+                                return ((PMarker)m).color.equals(Color.GREEN);
+                            }
+                            return false;
                         }
                         return true;
                     });
