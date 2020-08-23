1d8832d00176ba4e00382b626c63af23d0a0368f
elsid <elsid.mail@gmail.com>
2020-08-23 16:13:13 +0200

    Add dist build target

---
+++ etc/run.bat
@@ -0,0 +1,9 @@
+start javaw ^
+    -Xms512m ^
+    -Xmx1G ^
+    -Xss1M ^
+    -Dhaven.defserv=game.havenandhearth.com ^
+    -Dhaven.mapbase=http://game.havenandhearth.com/java/ ^
+    -Dhaven.resurl=https://game.havenandhearth.com/res/ ^
+    -Dhaven.cachebase=http://game.havenandhearth.com/render/ ^
+    -jar hafen.jar
---
+++ etc/run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+java \
+    -Xms512m \
+    -Xmx1G \
+    -Xss1M \
+    -Dhaven.defserv=game.havenandhearth.com \
+    -Dhaven.mapbase=http://game.havenandhearth.com/java/ \
+    -Dhaven.resurl=https://game.havenandhearth.com/res/ \
+    -Dhaven.cachebase=http://game.havenandhearth.com/render/ \
+    -jar hafen.jar
